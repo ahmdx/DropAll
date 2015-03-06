ca515d7c4a16cb708984cb6d9a8292bd10f6c448
RainbowZephyr <darkfeathers9@gmail.com>
2015-03-06 03:18:43 +0200

    added interface to main class

--- src/main/DBApp.java
+++ src/main/DBApp.java
@@ -1,10 +1,74 @@
 package main;
 
-public class DBApp {
+import java.util.Hashtable;
+import java.util.Iterator;
+
+import exceptions.DBAppException;
+import exceptions.DBEngineException;
+
+public class DBApp implements DBMainInterface{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void createTable(String strTableName,
+			Hashtable<String, String> htblColNameType,
+			Hashtable<String, String> htblColNameRefs, String strKeyColName)
+			throws DBAppException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void createIndex(String strTableName, String strColName)
+			throws DBAppException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void createMultiDimIndex(String strTableName,
+			Hashtable<String, String> htblColNames) throws DBAppException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insertIntoTable(String strTableName,
+			Hashtable<String, String> htblColNameValue) throws DBAppException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteFromTable(String strTableName,
+			Hashtable<String, String> htblColNameValue, String strOperator)
+			throws DBEngineException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Iterator selectFromTable(String strTable,
+			Hashtable<String, String> htblColNameValue, String strOperator)
+			throws DBEngineException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void saveAll() throws DBEngineException {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
