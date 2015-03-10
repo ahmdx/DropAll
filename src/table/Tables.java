bed0f52d11602e3ca5fc8ef874eb1ae1b1a01044
RainbowZephyr <darkfeathers9@gmail.com>
2015-03-10 22:39:01 +0200

    csv files

--- src/table/Tables.java
+++ src/table/Tables.java
@@ -1,6 +1,30 @@
 package table;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Hashtable;
+
+import exceptions.DBAppException;
+
 public class Tables {
+	private String format = "Table Name, Column Name, Column Type, Key, Indexed, ReferencesTable, ReferencesColumn";
+	private String[] formatList = format.split(",");
+
+	public Tables() {
+
+	}
+
+	public void createTable(String strTableName,
+			Hashtable<String, String> htblColNameType,
+			Hashtable<String, String> htblColNameRefs, String strKeyColName)
+			throws DBAppException, FileNotFoundException {
+		File csvFile = new File("metaFile.csv");
+		try(FileInputStream reader = new FileInputStream(csvFile))
+		;
+		
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
